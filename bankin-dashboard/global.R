28a3ee1cb9b987949017fceaa2c8d1d8d4ca2332
Mossa-D <mossa.diallo@gmail.com>
2021-11-10 15:29:33 +0100

    Prototype: data viz plots for month and total spending

--- bankin-dashboard/global.R
+++ bankin-dashboard/global.R
@@ -8,8 +8,8 @@ library(lubridate)
 library(data.table)
 library(DT)
 library(dplyr)
-
 library(reactlog)
+library(RColorBrewer)
 
 # tell shiny to log all reactivity
 reactlog_enable()
@@ -29,16 +29,18 @@ Preparing_Columns <- function(df) {
       Categorie = factor(Categorie),
       SousCategorie = factor(SousCategorie),
       Type = case_when(
-        SousCategorie %in% c("Virements internes") ~ "TRANSFERTS",
+        SousCategorie %in% "Virements internes" ~ "TRANSFERTS",
+        Categorie %in% "Investissement Epargne" ~ "TRANSFERTS",
         Categorie %in% "Entrées d'argent" ~ "CREDIT",
         TRUE ~ "DEBIT"),
-      Montant = if_else(Type == "DEBIT", -Montant, Montant),
+      Montant = if_else(Montant < 0, -Montant, Montant),
       AnneeMois = substr(Date, 1, 7))
   
   return(df)
 }
 
-df <- read_excel("bankin-dashboard/data/export_banques_2020-01-01_2021-10-31.xls", sheet = 1) %>% Preparing_Columns()
+df <- read_excel("bankin-dashboard/data/export_banques_2020-01-01_2021-10-31.xls", sheet = 1) %>% 
+  Preparing_Columns()
 
 Monthly_Groupby <- function(df) {
   # Monthly group by
@@ -48,19 +50,75 @@ Monthly_Groupby <- function(df) {
     arrange(AnneeMois) %>% 
     summarise(Montant = sum(Montant, na.rm = TRUE)) %>% 
     pivot_wider(names_from = Type, values_from = Montant) %>%
-    ungroup() %>% 
+    ungroup() %>%
     rename(Credit = CREDIT, Debit = DEBIT, Transferts = TRANSFERTS) %>% 
     mutate(
+      across(everything(), ~replace_na(.x, 0)),
       Balance = Credit - Debit,
       RunningBalance = cumsum(Balance),
-      TauxEpargne = Balance / Credit
+      TauxEpargne = Balance / Credit,
+      EvolCredit = (Credit - lag(Credit)) / Credit,
+      EvolDebit = (Debit - lag(Debit)) / Debit,
+      EvolTransferts = (Transferts - lag(Transferts)) / Transferts
     )
 }
 
 monthly_summary <- Monthly_Groupby(df)
-
 monthly_summary
+
   
-ggplot(Monthly_Groupby(df)) + 
-  geom_col(aes(x = AnneeMois, y = Transferts, group = 1)) + 
-  geom_line(aes(x = AnneeMois, y = RunningBalance, group=1))
+# Revenus / Dépenses totales du mois en cours
+Current_Month <- function(df) {
+  
+  
+}
+
+# Dépenses totales par catégorie sur l'année en cours
+
+df %>% 
+  group_by(Annee = year(Date), Categorie, Type) %>% 
+  summarise(Montant = sum(Montant)) %>% 
+  filter(Annee == 2020 & Type == "DEBIT") %>% 
+  
+  ggplot() + 
+  geom_col(aes(x = reorder(Categorie, -Montant), y = Montant, fill = Categorie)) + 
+  guides(fill = "none") +
+  labs(x="", title = "Repartition des dépenses") + 
+  theme(axis.text.x = element_text(angle = 90))
+
+
+# Revenus totaux par catégorie sur l'année en cours
+
+df %>% 
+  group_by(Annee = year(Date), SousCategorie, Type) %>% 
+  summarise(Montant = sum(Montant)) %>% 
+  filter(Annee == 2020 & Type == "CREDIT") %>% 
+  
+  ggplot() + 
+  geom_col(aes(x = reorder(SousCategorie, -Montant), y = Montant, fill = SousCategorie)) + 
+  guides(fill = "none") +
+  labs(x="", title = "Repartition des revenus") + 
+  scale_y_continuous(labels = scales::dollar_format()) + 
+  theme(axis.text.x = element_text(angle = 90))
+
+
+# Evolution des niveaux de dépenses / revenus mois par mois
+
+df %>% 
+  group_by(AnneeMois, Categorie, Type) %>% 
+  summarise(Montant = sum(Montant)) %>% 
+  filter(Type == "DEBIT") %>%
+  ggplot() +
+  geom_line(aes(x = AnneeMois, y = Montant, group = Categorie, color = Categorie)) +
+  labs(x="", title = "Evolution des dépenses par catégories") + 
+  theme(axis.text.x = element_text(angle = 90))
+
+df %>% 
+  group_by(AnneeMois, SousCategorie, Type) %>% 
+  summarise(Montant = sum(Montant)) %>% 
+  filter(Type == "CREDIT") %>%
+  ggplot() +
+  geom_col(aes(x = AnneeMois, y = Montant, group = SousCategorie, fill = SousCategorie)) +
+  scale_fill_brewer(palette = "Set1") +
+  labs(x="", title = "Evolution des revenus par sous-catégories") + 
+  theme(axis.text.x = element_text(angle = 90))
